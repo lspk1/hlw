5e92a120061e49000f89b275f4807e7667625ad3
lspk1 <151712004+lspk1@users.noreply.github.com>
2024-03-17 05:17:01 +0600

    Update run_gotty.sh

--- run_gotty.sh
+++ run_gotty.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# Start Xvfb
+Xvfb :0 -screen 0 1024x768x24 &
 
+# Start XFCE4 desktop environment
+startxfce4 &
+
+# Set up password for VNC server
+echo "123456" | vncpasswd -f > ~/.vnc/passwd
+chmod 600 ~/.vnc/passwd
+
+# Start VNC server
+x11vnc -display :0 -rfbauth ~/.vnc/passwd -forever &
+
+# Start GoTTY
 /usr/local/bin/gotty --permit-write --reconnect /bin/bash
